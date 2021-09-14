310986af6e5eff72f69988aeaef7f8adde7b102c
Maria Filipanova <myannyax@gmail.com>
2021-09-14 15:48:46 +0300

    fix bs

--- basic_algs/src/main.rs
+++ basic_algs/src/main.rs
@@ -30,29 +30,35 @@ fn n_prime(n: i32) -> i32 {
     }
 }
 
-fn search(numbers: &[i32; 10], n: i32) -> usize {
+fn search(numbers: &[i32; 10], n: i32) -> Option<usize> {
     let mut l = 0;
-    let mut r = 9;
+    let mut r = 10;
     while l < r {
         let m = (l + r) / 2;
         if numbers[m] < n {
             l = m + 1;
         } else if numbers[m] > n {
-            r = m - 1;
+            r = m;
         }
         else {
-            return m;
+            return Option::Some(m);
         }
     }
-    return 10;
+    return Option::None;
 }
 
 fn main() {
     let n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     println!("max = {}", maximum(&n));
-    println!("find 5's pos =  {}", search(&n, 5));
+    println!("find 5's pos =  {}", search(&n, 5).unwrap_or(999));
     for i in n {
         println!("{}'s prime = {}", i, n_prime(i));
     }
-    println!("Hello, world!");
+    for i in &n {
+        println!("{}'s index = {}", i, search(&n, *i).unwrap_or(999));
+    };
+    let n = [0, 2, 4, 5, 8, 9, 10, 11, 13, 18];
+    for i in &n {
+        println!("{}'s index = {}", i, search(&n, *i).unwrap_or(999));
+    };
 }
